af38b3486b128d99949f908bc4451310f6eb9544
root <root@SiddharthK.ClaimGeniusInc.com>
2023-07-17 19:01:38 +0530

    initial commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <NuxtPage />
+    <!-- <p class="text-8xl">Hello World</p> -->
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div
+      class="max-w-3xl max-w-5xlmin-h-screen flex justify-center mx-auto my-12"
+    >
+      <main class="w-full">
+        <h1 class="text-2xl font-semibold mb-6">My awesome blog</h1>
+        <section class="space-y-4 divide-y">
+          <article v-for="post of posts" :key="post.slug" class="pt-4">
+            <h2 class="text-lg mb-2 text-blue-700 hover:text-blue-800">
+              <NuxtLink :to="`/blog/${post.slug}`">
+                {{ post.title }}
+              </NuxtLink>
+            </h2>
+            <span>
+              {{ post.description }}
+            </span>
+          </article>
+        </section>
+      </main>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        posts: [],
+      }
+    },
+    async fetch() {
+      this.posts = await this.$content().fetch()
+    },
+  }
+  </script>
